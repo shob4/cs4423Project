aee613c6df760db95b48953be6d7d8195bc32603
shob4 <JonathonBonneau@gmail.com>
2023-04-05 02:03:30 -0500

    stopped child from transforming

--- Assets/CharacterRotator.cs
+++ Assets/CharacterRotator.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CharacterRotator : MonoBehaviour
 {
+    private Quaternion originalRotation;
     // Start is called before the first frame update
     void Start()
     {
-        
+      originalRotation = transform.rotation;        
     }
 
     // Update is called once per frame
@@ -21,12 +22,14 @@ public class CharacterRotator : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other){
       if (other.gameObject.CompareTag(otherObjectTag)){
-          transform.parent.Rotate(new Vector3(0f, rotationAmount, 0f));
+        transform.parent.Rotate(new Vector3(0f, rotationAmount, 0f));
+        transform.rotation = originalRotation;
       }
     }
     private void OnTriggerExit2D(Collider2D other){
       if (other.gameObject.CompareTag(otherObjectTag)){
         transform.parent.Rotate(new Vector3(0f, -rotationAmount, 0f));
+        transform.rotation = originalRotation;
       }
     }
 
